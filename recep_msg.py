741fc9792bcfa0b726860bd7760871aa4cb3876d
OuelMomo <74724616+OuelMomo@users.noreply.github.com>
2021-02-01 15:15:48 +0100

    Update recep_msg.py

--- recep_msg.py
+++ recep_msg.py
@@ -91,7 +91,7 @@ def recepStateVector(*arg):
         path_sequencing(g._AIRCRAFT,g._LISTPATHS[0],g._LISTPATHS[1]) # Au cas où il ne reste plus qu'un seul path dans la trajectoire
         if sequencing_conditions(g._AIRCRAFT,g._LISTPATHS[0]):
             s.sendActiveLeg(g._ACTIVELEG[0])
-            s.sendNewLegList(g._LEGLIST)
+            #s.sendNewLegList(g._LEGLIST)
             
         xtk_, tae_, dtwpt, bank_angle_ref = xtk(g._AIRCRAFT, g._LISTPATHS[0]), tae(g._AIRCRAFT, g._LISTPATHS[0]), g._AIRCRAFT.distance(g._TOWPT), bank_angle(g._AIRCRAFT, g._LISTPATHS[0], g._LISTPATHS[1])
         apdist=alongpath_distance(g._AIRCRAFT,g._LISTPATHS[0],g._LISTPATHS[1])

1067a40fa4572eaefae66e05dc4f94a196f3bfb8
OuelMomo <74724616+OuelMomo@users.noreply.github.com>
2021-02-01 15:11:02 +0100

    Update send_msg.py

--- send_msg.py
+++ send_msg.py
@@ -9,7 +9,8 @@ _TIME=0
 
 def sendActiveLeg(numSeq):
     IvySendMsg("GS_AL Time="+str(g._TIME)+" NumSeqActiveLeg="+str(numSeq))
-    
+
+'''
 def sendNewLegList(LegList):  #format de leg liste : "ID=WPT1 SEQ=0 COURSE=110  LAT= LON= ;ID=WPT2 SEQ=1 COURSE=150 LAT= LON=  ;............"
     C=""
     for i in range (len(LegList)):
@@ -30,7 +31,7 @@ def sendNewLegList(LegList):  #format de leg liste : "ID=WPT1 SEQ=0 COURSE=110  
         C+=" "
         C+=";"
     IvySendMsg("GS_AL Time="+str(g._TIME)+" LegList="+C)
-   
+'''
    
 def sendData(xtk, tae, dtwpt, bank_angle, alongpath_distance):
     IvySendMsg("GS_Data Time="+str(g._TIME)+" XTK="+str(xtk)+" TAE="+str(tae)+" DTWPT="+str(dtwpt)+" BANK_ANGLE_REF="+str(bank_angle)+" ALDTWPT="+str(alongpath_distance))

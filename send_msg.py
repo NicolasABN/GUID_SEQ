4860bc591e15bc4a4a9b6accb0654025c133511f
OuelMomo <74724616+OuelMomo@users.noreply.github.com>
2021-02-03 13:52:19 +0100

    Update send_msg.py

--- send_msg.py
+++ send_msg.py
@@ -7,35 +7,11 @@ from functions import *
 
 
 
-def sendActiveLeg(numSeq):
+def sendActiveLeg(numSeq):          #Envoie le numéro de séquencencement du leg actif sur le bus IVY
     IvySendMsg("GS_AL Time="+str(g._TIME)+" NumSeqActiveLeg="+str(numSeq))
 
-'''
-def sendNewLegList(LegList):  #format de leg liste : "ID=WPT1 SEQ=0 COURSE=110  LAT= LON= ;ID=WPT2 SEQ=1 COURSE=150 LAT= LON=  ;............"
-    C=""
-    for i in range (len(LegList)):
-        C+="ID="
-        C+=LegList[i][0]
-        C+=" "
-        C+="SEQ="
-        C+=LegList[i][1]
-        C+=" "
-        C+="COURSE="
-        C+=LegList[i][2]
-        C+=" "
-        C+="LAT="
-        C+=LegList[i][3]
-        C+=" "
-        C+="LON="
-        C+=LegList[i][4]
-        C+=" "
-        C+=";"
-    IvySendMsg("GS_AL Time="+str(g._TIME)+" LegList="+C)
-'''
    
-def sendData(xtk, tae, dtwpt, bank_angle, alongpath_distance):
+def sendData(xtk, tae, dtwpt, bank_angle, alongpath_distance):      #Envoie les infos calculées sur le bus IVY
     IvySendMsg("GS_Data Time="+str(g._TIME)+" XTK="+str(xtk)+" TAE="+str(tae)+" DTWPT="+str(dtwpt)+" BANK_ANGLE_REF="+str(bank_angle)+" ALDTWPT="+str(alongpath_distance))
     
     
-#def sendBankAngle(bankangle):
- #   IvySendMsg("")
